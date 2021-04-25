d31bd9e4bbeab43a22d2e8e44ec46d40b9207610
Tomek Plewniak <tomekplewniak@gmail.com>
2021-04-25 17:42:16 +0200

    Add a SignalR hub.

---
+++ ExploreCalifornia/ExploreCalifornia/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string text)
+        {
+            var message = new ChatMessage()
+            {
+                SenderName = name,
+                Text = text,
+                SentAt = DateTimeOffset.UtcNow
+            };
+
+            // Broadcast to all clients.
+            await Clients.All.SendAsync(
+                "ReceiveMessage",
+                message.SenderName,
+                message.SentAt,
+                message.Text);
+        }
+    }
+}
---
+++ ExploreCalifornia/ExploreCalifornia/Models/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExploreCalifornia.Models
+{
+    public class ChatMessage
+    {
+        public string SenderName { get; set; }
+
+        public string Text { get; set; }
+   
+        public DateTimeOffset SentAt { get; set; }
+    }
+}
